e21883d6bcc887f93a1f210646284f64420cd583
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2012-04-10 18:19:07 +0200

    rewrite emulator

--- Emulator.hs
+++ Emulator.hs
@@ -1,25 +1,54 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE NoMonomorphismRestriction, BangPatterns #-}
 
 module Emulator where
 
 import Data.Vector.Unboxed as V hiding ((++))
+import qualified Data.Vector.Unboxed.Mutable as VM
 import Data.Word
 import Data.Bits
+import qualified Data.List as L
 import qualified Data.ByteString as B
 import qualified Control.Monad.State.Strict as S
 import Control.Monad
+import Control.Monad.ST
 import System.Environment
 import Numeric
-import Prelude as P
+import Binary hiding ((.<<.), (.>>.))
+
+
+runSimpleTest :: IO ()
+runSimpleTest = do
+   let instr1 = bset bregA (blit 0x1)
+       instr2 = badd bregA (blit 0x2)
+       instr3 = bset bregB (blit 0x2)
+       instr4 = bsub bregA bregB
+
+   runDCPU $ dcpuFromList (instr1 ++ instr2 ++ instr3 ++ instr4)
+
+
+runDCPU :: DCPU_Data -> IO ()
+runDCPU dcpu = do
+   dcpu' <- execStep dcpu
+   putStrLn $ show dcpu'
+   runDCPU dcpu'
 
 
 execStep :: DCPU_Data -> IO DCPU_Data
 execStep dcpu = do
    let (instruc, dcpu') = S.runState readInstruction dcpu
    putStrLn $ show instruc
-   let dcpu'' = S.execState (execInstruction instruc) dcpu'
-   putStrLn $ show dcpu''
-   return dcpu''
+   return $! S.execState (execInstruction instruc) dcpu'
+
+
+dcpuFromList :: [Word16] -> DCPU_Data
+dcpuFromList binary =
+   DCPU_Data {ram = initRam binary, regs = V.replicate numRegs 0,
+              pc = 0, sp = stackBegin, ov = 0}
+   where
+      initRam = V.unfoldrN ramSize (\ls ->
+         if L.null ls
+            then Just (0, ls)
+            else Just (L.head ls, L.tail ls))
 
 
 execInstruction :: Instruction -> DCPU ()
@@ -35,8 +64,8 @@ execInstruction (BasicInstruction opcode (valA, locA) valB)
                             MUL -> let r = a*b in (r, r .>>. 16)
                             DIV -> if b == 0
                                       then (0, 0)
-                                      else let r = a `P.div` b
-                                           in (r, (a .<<. 16) `P.div` b)
+                                      else let r = a `div` b
+                                           in (r, (a .<<. 16) `div` b)
 
                             MOD -> if b == 0 then (0, 0) else (a `mod` b, 0)
                             SHL -> let r = a .<<. (toInt b) in (r, r .>>. 16)
@@ -68,7 +97,11 @@ execInstruction (BasicInstruction opcode (valA, locA) valB)
 
 execInstruction (NonBasicInstruction opcode val) = do
    case opcode of
-        JSR -> return ()
+        JSR -> S.modify (\dcpu ->
+           let sp' = sp dcpu - 1
+               in dcpu {ram = set (ram dcpu) sp' (pc dcpu + 1),
+                        sp = sp', pc = val})
+
         InvalidNonBasicOp -> error $ "Executing invalid non basic instruction!"
 
 
@@ -176,32 +209,32 @@ data NonBasicOpcode = InvalidNonBasicOp | JSR deriving (Show, Eq, Enum)
 type DCPU = S.State DCPU_Data
 
 data DCPU_Data = DCPU_Data {
-   ram  :: V.Vector Word16, -- ram
-   regs :: V.Vector Word16, -- registers
-   pc   :: Word16,          -- program counter
-   sp   :: Word16,          -- stack pointer
-   ov   :: Word16           -- overflow
+   ram  :: ! (V.Vector Word16), -- ram
+   regs :: ! (V.Vector Word16), -- registers
+   pc   :: ! Word16,            -- program counter
+   sp   :: ! Word16,            -- stack pointer
+   ov   :: ! Word16             -- overflow
    }
 
 instance Show DCPU_Data where
    show (DCPU_Data ram regs pc sp ov) =
-      "DCPU: regs=" ++ (show regs) ++ ", pc=" ++ (showHex pc "") ++
-      ", sp=" ++ (showHex sp "") ++ ", ov=" ++ (showHex ov "") ++
-      ", ram:" ++ showNonEmptyRam ram
+      "DCPU: regs=" ++ (V.foldl' (\str e -> (if str /= "[" then str ++ "," else str) ++ showHex e "") "[" regs) ++ "]" ++
+      ", pc=" ++ (showHex pc "") ++ ", sp=" ++ (showHex sp "") ++ ", ov=" ++ (showHex ov "")
 
-showNonEmptyRam ram = go ram 0
+
+showRam f ram = V.foldl' foldRam ("ram:", 0) ram
    where
-      go ram i
-         | i >= V.length ram = ""
-         | ram ! i == 0x0    = ""
-         | otherwise         = " [" ++ (showHex i "") ++ "]=" ++ (showHex (ram ! i) "")
+      foldRam (str, addr) cell =
+         if f cell
+            then (str ++ putCell addr cell, addr + 1)
+            else (str, addr + 1)
+
+      putCell addr cell = " [" ++ (showHex addr "]=") ++ (showHex cell "")
+
 
 ramSize    = 0x10000
 numRegs    = 8
 stackBegin = 0xffff
-
-newDCPU ram_ = DCPU_Data {ram = ram_, regs = V.replicate numRegs 0,
-                          pc = 0, sp = stackBegin, ov = 0}
 
 (.>>.) = shiftR
 (.<<.) = shiftL
@@ -214,5 +247,15 @@ setValue value SP             = S.modify (\dcpu -> dcpu {sp = value})
 setValue value O              = S.modify (\dcpu -> dcpu {ov = value})
 setValue value Literal        = return ()
 
-set vector index value = vector // [(fromIntegral index, value)]
+
+set :: Integral a => V.Vector Word16 -> a -> Word16 -> V.Vector Word16
+set vector index value = runST (setM vector (fromIntegral index) value)
+   where
+      setM vec i val = do
+         mVec <- V.unsafeThaw vec
+         VM.write mVec i val
+         V.unsafeFreeze mVec
+
+
+get :: Integral a => V.Vector Word16 -> a -> Word16
 get vector index = vector ! (fromIntegral index)
