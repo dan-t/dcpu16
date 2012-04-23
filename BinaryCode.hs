9720106977122fe7757356e95c1930a0e4632411
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2012-04-23 21:25:49 +0200

    Refactored BinaryCode.

--- BinaryCode.hs
+++ BinaryCode.hs
@@ -2,107 +2,134 @@
 
 module BinaryCode where
 
-import Data.Bits
+import Data.Bits hiding (xor)
 import Data.Char
 import Data.Word
-import Data.List
+import qualified Data.DList as DL
 import Control.Monad.Writer
 import Numeric
+import qualified Data.List as L
+import qualified CommonTypes as CT
+import Prelude hiding (div, mod, and)
 
-type BinaryCode = Writer [Word16]
-binaryCode = execWriter
+type BinaryCode = Writer (DL.DList Word16)
+binaryCode = DL.toList . execWriter
 
-setM = basicInstrucM 0x1
-addM = basicInstrucM 0x2
-subM = basicInstrucM 0x3
-mulM = basicInstrucM 0x4
-divM = basicInstrucM 0x5
-modM = basicInstrucM 0x6
-shlM = basicInstrucM 0x7
-shrM = basicInstrucM 0x8
-andM = basicInstrucM 0x9
-borM = basicInstrucM 0xa
-xorM = basicInstrucM 0xb
-ifeM = basicInstrucM 0xc
-ifnM = basicInstrucM 0xd
-ifgM = basicInstrucM 0xe
-ifbM = basicInstrucM 0xf
+setM = basicInstrucM set
+addM = basicInstrucM add
+subM = basicInstrucM sub
+mulM = basicInstrucM mul
+divM = basicInstrucM div
+modM = basicInstrucM mod
+shlM = basicInstrucM shl
+shrM = basicInstrucM shr
+andM = basicInstrucM and
+borM = basicInstrucM bor
+xorM = basicInstrucM xor
+ifeM = basicInstrucM ife
+ifnM = basicInstrucM ifn
+ifgM = basicInstrucM ifg
+ifbM = basicInstrucM ifb
 
-jsrM = nonBasicInstrucM 0x01
+basicInstrucM :: Word16 -> Word32 -> Word32 -> BinaryCode ()
+basicInstrucM opcode a b
+   | nextWordA && nextWordB = tell $ DL.fromList [firstWord, toW16 $ a .>>. 16, toW16 $ b .>>. 16]
+   | nextWordA              = tell $ DL.fromList [firstWord, toW16 $ a .>>. 16]
+   | nextWordB              = tell $ DL.fromList [firstWord, toW16 $ b .>>. 16]
+   | otherwise              = tell $ DL.fromList [firstWord]
+   where
+      firstWord = opcode .|. toW16 ((a .&. 0x3f) .<<. 4) .|. toW16 ((b .&. 0x3f) .<<. 10)
+      nextWordA = a .&. 0xffff0000 /= 0
+      nextWordB = b .&. 0xffff0000 /= 0
 
-regA = 0x00 :: Word32
-regB = 0x01 :: Word32
-regC = 0x02 :: Word32
-regX = 0x03 :: Word32
-regY = 0x04 :: Word32
-regZ = 0x05 :: Word32
-regI = 0x06 :: Word32
-regJ = 0x07 :: Word32
+jsrM = nonBasicInstrucM jsr
+
+nonBasicInstrucM :: Word16 -> Word32 -> BinaryCode ()
+nonBasicInstrucM opcode a
+   | nextWordA = tell $ DL.fromList [firstWord, secondWord]
+   | otherwise = tell $ DL.fromList [firstWord]
+   where
+      firstWord  = (opcode .<<. 4) .|. toW16 ((a .&. 0x3f) .<<. 10)
+      secondWord = toW16 $ a .>>. 16
+      nextWordA  = a .&. 0xffff0000 /= 0
+
+opcode opcode =
+   case opcode of
+        CT.SET -> set
+        CT.ADD -> add
+        CT.SUB -> sub
+        CT.MUL -> mul
+        CT.DIV -> div
+        CT.MOD -> mod
+        CT.SHL -> shl
+        CT.SHR -> shr
+        CT.AND -> and
+        CT.BOR -> bor
+        CT.XOR -> xor
+        CT.IFE -> ife
+        CT.IFN -> ifn
+        CT.IFG -> ifg
+        CT.IFB -> ifb
+
+nonBasicOpcodeM opcode =
+   case opcode of
+        CT.JSR -> jsrM
+
+reg name =
+   case name of
+        CT.A -> regA
+        CT.B -> regB
+        CT.C -> regC
+        CT.X -> regX
+        CT.Y -> regY
+        CT.Z -> regZ
+        CT.I -> regI
+        CT.J -> regJ
 
 ramAt :: Word32 -> Word32
 ramAt addr = (addr .<<. 16) .|. 0x1e
-
-pop  = 0x18 :: Word32
-peek = 0x19 :: Word32
-push = 0x1a :: Word32
-sp   = 0x1b :: Word32
-pc   = 0x1c :: Word32
-o    = 0x1d :: Word32
 
 lit :: Word32 -> Word32
 lit i
    | i > 0x1f  = (i .<<. 16) .|. 0x1f
    | otherwise = i + 0x20
 
+set = 0x1
+add = 0x2
+sub = 0x3
+mul = 0x4
+div = 0x5
+mod = 0x6
+shl = 0x7
+shr = 0x8
+and = 0x9
+bor = 0xa
+xor = 0xb
+ife = 0xc
+ifn = 0xd
+ifg = 0xe
+ifb = 0xf
 
-basicInstrucM :: Word16 -> Word32 -> Word32 -> BinaryCode ()
-basicInstrucM opcode a b = tell $ basicInstruc opcode a b
+jsr = 0x01
 
-nonBasicInstrucM :: Word16 -> Word32 -> BinaryCode ()
-nonBasicInstrucM opcode a = tell $ nonBasicInstruc opcode a
+pop  = 0x18
+peek = 0x19
+push = 0x1a
+sp   = 0x1b
+pc   = 0x1c
+o    = 0x1d
 
-basicInstruc :: Word16 -> Word32 -> Word32 -> [Word16]
-basicInstruc opcode a b
-   | nextWordA && nextWordB = [firstWord, toW16 $ a .>>. 16, toW16 $ b .>>. 16]
-   | nextWordA              = [firstWord, toW16 $ a .>>. 16]
-   | nextWordB              = [firstWord, toW16 $ b .>>. 16]
-   | otherwise              = [firstWord]
-   where
-      firstWord = opcode .|. toW16 ((a .&. 0x3f) .<<. 4) .|. toW16 ((b .&. 0x3f) .<<. 10)
-      nextWordA = a .&. 0xffff0000 /= 0
-      nextWordB = b .&. 0xffff0000 /= 0
+regA = 0x00
+regB = 0x01
+regC = 0x02
+regX = 0x03
+regY = 0x04
+regZ = 0x05
+regI = 0x06
+regJ = 0x07
 
-
-set = basicInstruc 0x1
-add = basicInstruc 0x2
-sub = basicInstruc 0x3
-mul = basicInstruc 0x4
-div = basicInstruc 0x5
-mod = basicInstruc 0x6
-shl = basicInstruc 0x7
-shr = basicInstruc 0x8
-and = basicInstruc 0x9
-bor = basicInstruc 0xa
-xor = basicInstruc 0xb
-ife = basicInstruc 0xc
-ifn = basicInstruc 0xd
-ifg = basicInstruc 0xe
-ifb = basicInstruc 0xf
-
-nonBasicInstruc :: Word16 -> Word32 -> [Word16]
-nonBasicInstruc opcode a
-   | nextWordA = [firstWord, secondWord]
-   | otherwise = [firstWord]
-   where
-      firstWord  = (opcode .<<. 4) .|. toW16 ((a .&. 0x3f) .<<. 10)
-      secondWord = toW16 $ a .>>. 16
-      nextWordA  = a .&. 0xffff0000 /= 0
-
-jsr = nonBasicInstruc 0x01
-
-
-hex = intercalate " " . map (\w -> showHex w "")
-bin = intercalate " " . map (\w -> showIntAtBase 2 intToDigit w "")
+hex = L.intercalate " " . L.map (\w -> showHex w "")
+bin = L.intercalate " " . L.map (\w -> showIntAtBase 2 intToDigit w "")
 
 (.>>.) = shiftR
 (.<<.) = shiftL
